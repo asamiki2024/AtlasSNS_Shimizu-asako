4ef13c6ecbe8a336a5b1b524924927a081fca8ca
asako202404 <disney.asako@icloud.com>
2024-07-24 00:36:43 +0900

    投稿フォーム

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。</h2>
-
+<h2>投稿内容を入力してください。</h2>
+<div class="container">
+    <!--投稿フォーム  -->
+    <div class="form-group">
+        <textarea name="request-about" id="request-about" placeholder="投稿内容を入力してください。"></textarea>
+        <!-- 送信ボタン -->
+        <button class="form-button"><img src="images/post.png" /></button>
+    </div>
+</div>
 @endsection
